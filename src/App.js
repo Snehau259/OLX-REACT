032a6e9a3cb6dc620d7e36324b8d09739f44b901
snehau259 <snehau259@gmail.com>
2023-01-24 23:11:40 +0530

    [Sneha] add. authStateChanged

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Signup from './Components/Signup/Signup'
 import Login from './Components/Login/Login'
-/**
- * ?  =====Import Components=====
- */
+import { AuthContext, FirebaseContext } from './store/FirebaseContext';
 import Home from './Pages/Home';
 
+
+
+
 function App() {
+  const { firebase } = useContext(FirebaseContext)
+  const { setUser } = useContext(AuthContext)
+  useEffect(() => {
+    try {
+      firebase.auth().onAuthStateChanged((user) => { setUser(user) });
+    } catch (error) {
+      console.log('error is' + error.message);
+    }
+  });
+  
   return (
     <div>
       <Router>
